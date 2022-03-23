8e2356aef4c5d4103b5827d7c26dadf3651b4004
Irungaray <lionelirungaray@gmail.com>
2022-03-23 01:39:42 -0300

    Add CORS middleware

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -1,9 +1,10 @@
 // dotenv
 require('dotenv').config()
-let { PORT } = process.env
+let { PORT, clientUri } = process.env
 
 // Ext modules
 import express from 'express'
+import cors  from 'cors'
 
 // Int modules
 import { deserializeUser } from './middlewares/deserializeUser'
@@ -12,9 +13,13 @@ import { connect } from './utils/connect'
 import { logger } from './utils/logger'
 import { routes } from './routes'
 
+logger.warn("BACKEND")
+
 const app = express()
 
 app.use(express.json())
+app.use(cors({ origin: clientUri }))
+
 app.use(deserializeUser)
 
 app.listen(PORT, async () => {
